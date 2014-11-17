7f9fbb910db6e55ca57d4a8ff92afbdd8166000e
nrlakin <neil.lakin@gmail.com>
2014-11-17 14:07:07 -0500

    Still need postgres working

--- wah_test/views.py
+++ wah_test/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Greeting
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('WeAreHere placeholder.')
 
 
 def db(request):
