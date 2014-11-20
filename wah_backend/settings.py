9fedb7d6e0f29d8bad07288b7919a48b804958a1
nrlakin <neil.lakin@gmail.com>
2014-11-20 13:26:10 -0500

    update db settings

--- wah_backend/settings.py
+++ wah_backend/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'wah_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wearehere_db',
+        'USER': 'wah_admin',
+        'PASSWORD': 'jdktawnrl',
+        'HOST': ",
+        'PORT': ",
     }
 }
 
