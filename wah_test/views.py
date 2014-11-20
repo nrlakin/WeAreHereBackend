b82fe3dddc39241947f9d287fd15458e733c001c
nrlakin <neil.lakin@gmail.com>
2014-11-20 15:51:45 -0500

    trying to get post..."
    quit
    quit()
    \q
    "

--- wah_test/views.py
+++ wah_test/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-import requests
+from rest_framework.views import APIView
 
 from .models import Greeting
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
-        return HttpResponseRedirect('/hello/')
 
-    else:
+class WAH_Test(APIView):
+
+    def post(self, request):
+        return HttpResponse("/got it/")
+
+    def index(self, request):
+
         return HttpResponse('WeAreHere placeholder.')
 
 
-def db(request):
+    def db(self, request):
 
-    greeting = Greeting()
-    greeting.save()
+        greeting = Greeting()
+        greeting.save()
 
-    greetings = Greeting.objects.all()
+        greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+        return render(request, 'db.html', {'greetings': greetings})
