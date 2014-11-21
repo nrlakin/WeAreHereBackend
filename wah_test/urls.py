1b40ab86860890900e904e142926cd3806780ca0
Neil <neil.lakin@gmail.com>
2014-11-21 00:04:41 -0500

    fixing import errors

--- wah_test/urls.py
+++ wah_test/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from wah_test import views
 
-urls = [
+urlpatterns = [
     url(r'^occupancy/$', views.occupancy),
     url(r'^update/(?P<id>[0-9]+)/$', views.update),
 ]
