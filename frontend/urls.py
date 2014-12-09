68b53c8b0af5ca6dcd21dd65f09f13e5a152f977
Neil Lakin <Neil@dhcp-ccc-399.redrover.cornell.edu>
2014-12-08 22:58:25 -0500

    delete some unused files

--- frontend/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from test_frontend import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
--- frontend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-
-# Create your views here.
-def index(request):
-    return Response("Test frontend.")
