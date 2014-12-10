800b1fc042779e807baaa1448f32afb8796174b4
Neil Lakin <Neil@dhcp-ccc-399.redrover.cornell.edu>
2014-12-10 00:45:48 -0500

    reorder filters in occupancy view

--- wah_test/views.py
+++ wah_test/views.py
@@ -85,7 +85,7 @@ class Occupancy(generics.ListAPIView):
     serializer_class = CheckInSerializer
 
     def get_queryset(self):
-        return CheckIn.objects.exclude(room_id__lt=1).order_by('user', '-when').distinct('user')
+        return CheckIn.objects.order_by('user', '-when').distinct('user').exclude(room_id__lt=1)
 
 def register(request):
     """
